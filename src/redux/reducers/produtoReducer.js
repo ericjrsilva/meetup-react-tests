0843906719a9c5ea5b2fe4362030186decba691f
Mateus de Souza Costa <mateus.costa@SLT-002255.solutis.net.br>
2019-05-09 13:44:54 -0300

    Feature teste Reducer

---
+++ src/redux/reducers/produtoReducer.js
@@ -0,0 +1,51 @@
+const initialState = {
+    produtos: [],
+    isShowModalCadastro: false,
+    produto: {},
+    index: null
+};
+
+export default function (state = initialState, action) {
+    switch(action.type) {
+        case 'GET_PRODUTOS':
+            return {
+                ...state,
+                produtos: action.payload
+            };
+        case 'SHOW_MODAL_CADASTRO':
+            return {
+                ...state,
+                isShowModalCadastro: true,
+                produto: action.payload.produto,
+                index: action.payload.index
+            }
+        case 'HIDE_MODAL_CADASTRO':
+            return {
+                ...state,
+                isShowModalCadastro: false,
+                produto: {}
+            }
+        case 'SAVE':
+            var listaAtividades = state.produtos
+
+            if (action.payload.index !== null)
+                listaAtividades[action.payload.index] = action.payload.produto.values
+            else
+                listaAtividades = listaAtividades.concat(action.payload.produto.values)
+
+            return {
+                ...state,
+                produtos: listaAtividades,
+                isShowModalCadastro: false,
+                produto: {},
+                index: null
+            }
+        case 'EXCLUIR':
+            return {
+                ...state,
+                produtos: [...state.produtos.filter(prod => prod !== action.payload)]
+            }
+        default:
+            return state;
+    }
+}
